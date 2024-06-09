bde078d297cda2bef52d68cd4bd732aa5d86a403
Steve Chikwiri <37661480+mijiga@users.noreply.github.com>
2024-06-09 19:02:25 +0300

    feat: ios setup

--- ios/ZendeskLiveChat.mm
+++ ios/ZendeskLiveChat.mm
@@ -6,6 +6,12 @@ RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 
+RCT_EXTERN_METHOD(initZendesk:(NSString)accountKey
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(openZendeskChat)
+
 + (BOOL)requiresMainQueueSetup
 {
   return NO;
--- ios/ZendeskLiveChat.swift
+++ ios/ZendeskLiveChat.swift
@@ -1,8 +1,40 @@
+import ZendeskSDKMessaging
+import ZendeskSDK
+
 @objc(ZendeskLiveChat)
 class ZendeskLiveChat: NSObject {
+    
+    @objc(multiply:withB:withResolver:withRejecter:)
+    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve(a*b)
+    }
+    
+    @objc(initZendesk:withResolver:withRejecter:)
+    func initZendesk(apiKey: String,resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        Zendesk.initialize(withChannelKey: apiKey, messagingFactory: DefaultMessagingFactory()) { result in
+            if case let .failure(error) = result {
+                print("Messaging did not initialize.\nError: \(error.localizedDescription)")
+            } else {
+                print("Successfully initialized")
+            }
+        }
+    }
+    
+    @objc(openZendeskChat)
+    func openZendeskChat() -> Void {
+        DispatchQueue.main.async {
+            guard let viewController = Zendesk.instance?.messaging?.messagingViewController() else { return }
+                
+            let keyWindow = UIApplication.shared.connectedScenes
+                .filter({$0.activationState == .foregroundActive})
+                .map({$0 as? UIWindowScene})
+                .compactMap({$0})
+                .first?.windows
+                .filter({$0.isKeyWindow}).first
 
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
+            let rootViewController = keyWindow?.rootViewController
+
+            rootViewController?.present(viewController, animated: true, completion: nil)
+        }
+    }
 }
